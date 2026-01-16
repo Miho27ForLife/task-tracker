3044254049d3275665e7aabe3ac2226054d82c15
Mohamed Bouichou <mohamed.bouichou1@gmail.com>
2026-01-16 11:14:05 +0100

    Initial task tracker

---
+++ script.js
@@ -0,0 +1,17 @@
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+addTaskBtn.addEventListener("click", function () {
+    const taskText = taskInput.value;
+
+    if (taskText === "") {
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    taskList.appendChild(li);
+    taskInput.value = "";
+});
